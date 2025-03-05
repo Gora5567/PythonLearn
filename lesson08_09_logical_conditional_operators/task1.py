ea98a75d22c2eb7e3670c2188926778dfa7cbfcf
egormarik <egormarikpt@gmail.com>
2025-03-05 17:56:25 +0200

    edited task1.py edited task2.py

--- lesson08_09_logical_conditional_operators/task1.py
+++ lesson08_09_logical_conditional_operators/task1.py
@@ -3,10 +3,9 @@
 from helper import *
 
 start_task(1)
+input_num = input("Enter any number: ")
 
-something = input("Enter any number: ")
-
-if something == "1":
+if input_num == "7":
     print("7 is a lucky number! Today is your lucky day!")
 else:
     print('Thank you! Have a nice day!')
