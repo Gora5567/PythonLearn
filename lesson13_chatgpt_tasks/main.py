ca86b51972e7aad4d89bd938431d11a91ec9ffbd
egormarik <egormarikpt@gmail.com>
2025-05-14 20:21:46 +0300

    added new tasks

--- lesson13_chatgpt_tasks/main.py
+++ lesson13_chatgpt_tasks/main.py
@@ -1,10 +1,16 @@
 from helpers.helper import *
-from lesson13_chatgpt_tasks import task1, task2
+from lesson13_chatgpt_tasks import task1, task2, task3, task4
 
 
-# input_data = int(input("Enter number which indicates square size:\n"))
-# runner(task1.print_square, input_data)
+input_data = int(input("Enter number which indicates square size:\n"))
+runner(task1.print_square, input_data)
 
 input_data1 = int(input("Enter number which indicates what number will the multiplication"
                         " table go up to\n"))
 runner(task2.multiplication_table, input_data1)
+
+input_data2 = int(input("Enter number which indicates what number will the number stairs go up to\n"))
+runner(task3.number_stairs, input_data2)
+
+input_data3 = int(input("Enter number which indicates what number will the number pyramide go up to\n"))
+runner(task4.draw_pyramid, input_data3)
---
+++ lesson13_chatgpt_tasks/task4.py
@@ -0,0 +1,9 @@
+def draw_pyramid(n):
+    width = n + n - 1
+    value_part = 1
+
+    for i in range(1, n + 1):
+        emptySide = int((width - value_part) / 2) ## empty cell count in the row
+
+        print(" " * emptySide + "*" * value_part + " " * emptySide)
+        value_part += 2
