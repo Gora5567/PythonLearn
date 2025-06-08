a5d6215f752b3a5000380c2e768ee832b5aa0cd1
egormarik <egormarikpt@gmail.com>
2025-06-08 19:49:31 +0300

    edited task6_callable_test.py

--- lesson14_15_functions/task6_callable_test.py
+++ lesson14_15_functions/task6_callable_test.py
@@ -1,7 +1,7 @@
 from task6_callable import execute_if_iterable_and_callable
 def hello():
-    print("Привет!")
+    print("hello!")
 
-items = [hello, 52, "ыыы"]
+items = [hello, 52, "sss"]
 
 execute_if_iterable_and_callable(items)
