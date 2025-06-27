92c7fed5bfb410a7f05380ba9f2f339cf0dff5d3
egormarik <egormarikpt@gmail.com>
2025-06-27 16:31:58 +0300

    Made task6_callable_test.py work, not finished yet

--- lesson14_15_functions/task6_callable.py
+++ lesson14_15_functions/task6_callable.py
@@ -1,18 +1,15 @@
 from collections.abc import Iterable
 
-
 def execute_if_iterable_and_callable(value):
     if not isinstance(value, Iterable) or isinstance(value, (str, bytes)):
         print("Переменная не является итерируемой.")
         return
 
     for item in value:
-        if not callable(item):
-            continue
-
-        try:
-            return item()
-            continue
-
-        except Exception as e:
-            print(f"Ошибка при вызове: {e}")
+        if callable(item):
+            try:
+                result = item()
+                if result is not None:
+                    print(result)
+            except Exception as e:
+                print(f"Ошибка при вызове: {e}")
--- lesson14_15_functions/task6_callable_test.py
+++ lesson14_15_functions/task6_callable_test.py
@@ -1,14 +1,30 @@
+import sys
+import io
 from task6_callable import execute_if_iterable_and_callable
 
 def test_execute_if_iterable_and_callable():
+    def capture_output(value):
+        old_stdout = sys.stdout
+        sys.stdout = buffer = io.StringIO()
+        try:
+            execute_if_iterable_and_callable(value)
+        finally:
+            sys.stdout = old_stdout
+        return buffer.getvalue().strip()
+
     def hello():
-        return "hello!"
+        print("hello!", end='')
+
     def hello2():
-        return "hello2"
+        print("hello2",end='')
+
+    def yo():
+        print("yo!", end='')
 
     items = [hello, 52, "sss"]
-    items2 = [hello2, 52, hello2]
+    items2 = [hello, 52, hello2]
+    items3 = [hello2, hello, yo, "sss"]
 
-    assert execute_if_iterable_and_callable(items) == "hello!"
-    assert execute_if_iterable_and_callable(items) == "hello!"
-
+    assert capture_output(items) == "hello!"
+    assert capture_output(items2) == "hello!hello2"
+    assert capture_output(items3) == "hello2hello!yo!"
