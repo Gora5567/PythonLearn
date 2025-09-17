bacbdc729b5656c5ef1edc3c9022c5deae99ca2c
egormarik <egormarikpt@gmail.com>
2025-09-17 15:19:40 +0300

    wip

---
+++ lesson17_map_filter_lambda/task6_strlen_lambd_test.py
@@ -0,0 +1,24 @@
+from task6_strlen_lambd import last_char
+
+def test_last_char():
+    test_cases = [
+        {
+            "in": "hello",
+            "out": "o"
+        },
+        {
+            "in": "Python",
+            "out": "n"
+        },
+        {
+            "in": "a",
+            "out": "a"
+        },
+        {
+            "in": "12345",
+            "out": "5"
+        },
+    ]
+
+    for case in test_cases:
+        assert last_char(case["in"]) == case["out"]
