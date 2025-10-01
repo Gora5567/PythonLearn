ea155274d7c005d64da6d0b5ceb1e1b1ca8c2529
egormarik <egormarikpt@gmail.com>
2025-10-01 17:26:34 +0300

    fixed test

--- lesson16_args_kwargs/kwargs/task2_print_info_test.py
+++ lesson16_args_kwargs/kwargs/task2_print_info_test.py
@@ -1,6 +1,6 @@
 from task2_print_info import print_info
 
-
-def test_print_info(**kwargs):
-    assert print_info(name='Jack', age=20) == {'name': 'Jack', 'age': 20}
-    assert print_info(name='Mike', age=20) == {'name': 'Mike', 'age': 20}
+def test_print_info(capsys):
+    print_info(name='Jack', age=20)
+    out, err = capsys.readouterr()
+    assert out == 'name: Jack\nage: 20\n'
